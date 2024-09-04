58f4758619c9316281485629bd9e87ae6f2d7710
Ira <nikulochkina.irina@yandex.ru>
2024-09-04 14:18:06 +0300

    added resiseOserver hook

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import cn from 'classnames';
+import { useResizeObserver } from '../../shared/lib/hooks';
 import { TodoList } from '../../modules/TodoList';
 import styles from './MainPage.module.scss';
 import { AddTodoItem } from '../../modules/AddTodoItem';
@@ -15,13 +17,20 @@ export const MainPage = () => {
     const { currentTheme } = useThemeContext();
     const backgroundColor = getBackgroundColor(currentTheme);
 
+    const { observedElementRef, size } = useResizeObserver();
+
+    const isVertical = size.width < 1200;
+
     return (
         <div className={themeStyles[backgroundColor]}>
             <main className={styles.mainPage}>
                 <Header />
                 <Search />
                 <TodoFilter />
-                <section className={styles.mainSection}>
+                <section
+                    className={cn(styles.mainSection, { [styles.verticalLayout]: isVertical })}
+                    ref={observedElementRef}
+                >
                     <div>
                         <CommentList />
                         <AddComment />
