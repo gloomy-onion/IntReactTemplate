b3d3235ccbf2b3bebc2fbe3c259939c7eea7fe50
irina.nikulochkina <irina.nikulochkina@redcollar.ru>
2026-03-09 11:48:25 +0300

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,40 +4,32 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
 export default () => {
-  return defineConfig({
-    build: {
-      outDir: './build'
-    },
-    plugins: [
-      react({
-        exclude: /\.stories\.(t|j)sx?$/,
-        include: '**/*.tsx',
-      }),
-      tsconfigPaths(),
-      svgrPlugin({
-        svgrOptions: {
-          icon: false,
+    return defineConfig({
+        base: '/IntReactTemplate/',
+        build: {
+            outDir: './build'
         },
-      }),
-    ],
-    server: {
-      host: 'localhost',
-      port: 3000,
-      /*
-      proxy: {
-          '/api': {
-              changeOrigin: true,
-              target: 'https://your.host/',
-              secure: false,
-          },
-      },
-      cors: false,*/
-    },
-    esbuild: {
-      define: {
-        this: 'window'
-      },
-      jsxFactory: `jsx`,
-    },
-  })
+        plugins: [
+            react({
+                exclude: /\.stories\.(t|j)sx?$/,
+                include: '**/*.tsx',
+            }),
+            tsconfigPaths(),
+            svgrPlugin({
+                svgrOptions: {
+                    icon: false,
+                },
+            }),
+        ],
+        server: {
+            host: 'localhost',
+            port: 3000,
+        },
+        esbuild: {
+            define: {
+                this: 'window'
+            },
+            jsxFactory: `jsx`,
+        },
+    })
 }
